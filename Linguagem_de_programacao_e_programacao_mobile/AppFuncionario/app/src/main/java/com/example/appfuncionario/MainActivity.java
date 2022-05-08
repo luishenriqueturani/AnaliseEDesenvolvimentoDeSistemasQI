bab62cb4e05884034964aeb99f1862c056e47e09
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2022-05-07 22:36:02 -0300

    terminado

--- Linguagem_de_programacao_e_programacao_mobile/AppFuncionario/app/src/main/java/com/example/appfuncionario/MainActivity.java
+++ Linguagem_de_programacao_e_programacao_mobile/AppFuncionario/app/src/main/java/com/example/appfuncionario/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.appfuncionario;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText nome = (EditText) findViewById(R.id.txt_nome);
+        EditText cargo = (EditText) findViewById(R.id.txt_cargo);
+        EditText telefone = (EditText) findViewById(R.id.txt_telefone);
+
+        Button btnGravar = (Button) findViewById(R.id.btn_gravar);
+
+        btnGravar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "Nome: "+nome.getText()+"\nCargo: "+cargo.getText()+"\nTelefone: "+telefone.getText();
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+
+                toast.show();
+            }
+        });
     }
+
+
 }
