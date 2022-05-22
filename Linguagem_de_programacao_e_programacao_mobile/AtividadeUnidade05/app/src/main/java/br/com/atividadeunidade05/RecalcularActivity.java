cdfc98315606ddf3fa6e1d79f9061607d820be0d
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2022-05-22 11:34:15 -0300

    Base feita
    Criada as telas e feito os estilos

---
+++ Linguagem_de_programacao_e_programacao_mobile/AtividadeUnidade05/app/src/main/java/br/com/atividadeunidade05/RecalcularActivity.java
@@ -0,0 +1,14 @@
+package br.com.atividadeunidade05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecalcularActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recalcular);
+    }
+}
